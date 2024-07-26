6973faac00982825e2e378a792953c78ae0b068e
Hamlet <115415995+HamletSargsyan@users.noreply.github.com>
2024-07-26 11:40:23 +0000

    changes

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # tumo_nodejs_telegram_bot_lab
+
+## Usage
+
+```bash
+npm install
+
+npm run prod # for production
+
+npm run dev # for development
+```
+
+## Environment variables
+
+BOT_TOKEN: required (get from https://t.me/BotFather)
+OPENWEATHER_API_KEY: required (get form https://openweathermap.org/api)
--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 const TelegramBot = require('node-telegram-bot-api');
+const axios = require("axios")
+
 require('dotenv').config()
 
 
 const token = process.env.BOT_TOKEN;
+const openweather_api_key = process.env.OPENWEATHER_API_KEY
+
+const DEFAULT_CITY = 'Yerevan';
 
 const bot = new TelegramBot(token, { polling: true });
 
@@ -12,6 +17,14 @@ const commands = [
     command: "start",
     description: "start"
   },
+  {
+    command: "weather",
+    description: "weather"
+  },
+  {
+    command: "set_city",
+    description: "set city"
+  }
 ]
 
 bot.setMyCommands(commands)
@@ -21,13 +34,28 @@ const defaultOptions = {
 }
 
 
+let users_info = {}
+
 
 /* -------------------------------------------------------------------------- */
 /*                                    utils                                   */
 /* -------------------------------------------------------------------------- */
 
-function randint(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
+async function getWeather(city) {
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${openweather_api_key}&units=metric`;
+  try {
+    const response = await axios.get(url);
+    if (response.status_code) { }
+    const weatherData = response.data;
+    return weatherData;
+  } catch (error) {
+    throw error;
+  }
+};
+
+
+function textEq(text1, text2) {
+  return true ? text1 === text2 : false;
 }
 
 
@@ -37,20 +65,8 @@ function randint(min, max) {
  * @returns {boolean} 
  */
 function commandEq(message, command) {
-  return true ? message.text === `/${command}` : false
+  return true ? message.text.toLowerCase().trim().split(" ")[0] === `/${command}` : false
 }
-
-
-/**
- * @param {TelegramBot.Message} message 
- * @param {String} text 
- * @returns {boolean}
- */
-function textEq(message, text) {
-  return true ? message.text.toLowerCase() === text.toLowerCase() : false
-}
-
-
 
 /* -------------------------------------------------------------------------- */
 /*                                  handlers                                  */
@@ -58,13 +74,57 @@ function textEq(message, text) {
 
 
 bot.on('text', (message) => {
-  const chatId = message.chat.id;
+  const chatId = message.from.id;
+  let args = message.text.toLowerCase().trim().split(" ")
 
-  switch (message.text.toLowerCase().trim()) {
-    case "/start":
+  if (commandEq(message, "start")) {
+    bot.sendMessage(chatId, "start cmd")
+  } else if (commandEq(message, "weather")) {
+    let city;
+    if (args.length > 1) {
+      city = args[1]
+    } else {
+      if (chatId in users_info) {
+        city = users_info[chatId]
+      } else {
+        city = DEFAULT_CITY
+      }
+    }
 
-      bot.sendMessage(chatId, "start cmd")
-      break;
+    getWeather(city)
+      .then(weather => {
+        mess = `<b>Weather in ${city}</b>\n\n`
+        mess += "<b><u>[weather]</u></b>\n"
+        mess += `<b>description:</b> ${weather.weather[0].description}\n`
+        mess += `<b>temp:</b> ${weather.main.temp}°C\n`
+        mess += `<b>min temp:</b> ${weather.main.temp_min} | <b>max temp:</b> ${weather.main.temp_max}\n\n`
+        mess += "<b><u>[wind]</u></b>\n"
+        mess += `<b>speed:</b> ${weather.wind.speed} meter/sec`
+
+        bot.sendMessage(chatId, mess, defaultOptions)
+      })
+      .catch(error => {
+        if (error.response.status === 404) {
+          bot.sendMessage(chatId, "Unknown city")
+          return
+        }
+        bot.sendMessage(chatId, `<b>ERROR</b>: ${error}`, defaultOptions)
+      });
+  } else if (commandEq(message, "set_city")) {
+    if (args.length === 1) {
+      mess = `<b>Usage:</b> <code>${args[0]} [city name]</code>`
+      bot.sendMessage(chatId, mess, defaultOptions)
+      return
+    }
+
+    users_info[chatId] = args[1]
+    bot.sendMessage(chatId, "Done ✅")
   }
 })
+
+
+
+
+
+
 
